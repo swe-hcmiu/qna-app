c8d34752cabcfa4a97bcd5ef6f2fb325e118503c
ToBaThanhTung <tungtoitiu@gmail.com>
2018-10-17 23:06:18 +0700

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/authenticate/Login';
+import Register from './components/authenticate/Register';
+import Session from './components/session/Session';
 import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={  Landing } />
+          <div className="container">
+          <Route exact path="/register" component={  Register } />
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/sessions" component={ Session } />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
@@ -10,16 +11,16 @@ class Landing extends Component {
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Question And Anwser</h1>
                 
-                <input className="textbox" type="text" placeholder="Find Your Session" />
+                <input className="textbox col align-self-center" type="text" placeholder="Find Your Session" />
                 <input title="Search" value="" type="submit" className="button"/>
                
                 <hr /> 
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-3">
                   Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-              </a>
+              </Link>
               </div>
             </div>
           </div>
